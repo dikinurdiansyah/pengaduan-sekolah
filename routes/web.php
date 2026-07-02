edf8b875d526b1cd62b206d74a781fd4517c4efa
Diki Nuriansah <dikinurdiansyah15@example.com>
2026-07-03 00:03:11 +0700

    Fix: Valid routes & remove missing auth require

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Halaman Utama Onboarding
 Route::get('/', function () {
     return view('onboarding');
 });
 
-require __DIR__.'/auth.php';
-
+// Route Admin Dashboard
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard');
 });
